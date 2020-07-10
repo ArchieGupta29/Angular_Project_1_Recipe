252f0e25569a161b321063cf963c85b5bd2b98f5
archiegupta <archiegupta99@gmail.com>
2020-07-10 15:30:39 +0530

    observables

--- angular-project/src/app/recipes/recipe.service.ts
+++ angular-project/src/app/recipes/recipe.service.ts
@@ -1,12 +1,12 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
 
 @Injectable()
 export class RecipeService {
 
-    recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
         new Recipe(
             'Tasty Schnitzel', 
@@ -15,13 +15,26 @@ export class RecipeService {
             [
                 new Ingredient('Meat', 1),
                 new Ingredient('French Fries', 20)
-            ] )
+            ] ),
+        new Recipe(
+                'Big Fat Burger', 
+                'What else do you need to say',
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg/1200px-Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+                [
+                    new Ingredient('Buns', 2),
+                    new Ingredient('Meat', 1),
+                    new Ingredient('Cheese', 1)
+                ] )
       ];
 
       constructor(private slService: ShoppingListService){}
 
       getRecipes() {
           return this.recipes.slice();
+      }
+
+      getRecipe(index: number) {
+          return this.recipes[index];
       }
     
       addIngredientsToShoppingList(ingredients: Ingredient[]) {
